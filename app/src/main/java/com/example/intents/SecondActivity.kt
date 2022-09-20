9d50c8b203829151470b110ed36145291624312e
naimgomezcn@gmail.com <naimgomezcn@gmail.com>
2022-09-20 19:27:04 +0200

    h

--- app/src/main/java/com/example/intents/SecondActivity.kt
+++ app/src/main/java/com/example/intents/SecondActivity.kt
@@ -1,7 +1,11 @@
 package com.example.intents
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.widget.RadioGroup
+import android.widget.SeekBar
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.intents.databinding.ActivitySecondBinding
@@ -16,7 +20,44 @@ class SecondActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val bundle: Bundle? = intent.extras
-        val personName = bundle?.getString("personName")
+        binding.radioGroup.check(binding.radioButtonHola.id)
 
+        val personName = bundle?.getString("personName")
+        var personAge = ""
+        var selectedString = "";
+
+        binding.ageSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                personAge = p1.toString()
+                binding.ageNumber.text = personAge
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+        } )
+
+        binding.radioGroup.setOnCheckedChangeListener { _, p1 ->
+            if (p1 == 1)
+                selectedString = getString(R.string.hola);
+            if (p1 == 2)
+                selectedString = getString(R.string.adeu);
+        }
+
+        binding.goNextButton.setOnClickListener {
+
+            val intent = Intent(this, ThirdActivity::class.java)
+
+            intent.putExtra("personAge", personAge)
+            intent.putExtra("personName", personName)
+            intent.putExtra("selectedStr", personName)
+
+            Toast.makeText(this, selectedString, Toast.LENGTH_SHORT).show()
+
+            startActivity(intent)
+        }
     }
 }
